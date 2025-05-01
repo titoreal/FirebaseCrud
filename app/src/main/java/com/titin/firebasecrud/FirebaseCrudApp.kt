1d782a639eceeb6c623e13169ae5fe1e509622e0
titin <titintr@hotmail.com>
2025-05-01 00:51:04 -0500

    Codificando clasers basicas

--- app/src/main/java/com/titin/firebasecrud/FirebaseCrudApp.kt
+++ app/src/main/java/com/titin/firebasecrud/FirebaseCrudApp.kt
@@ -1,4 +1,16 @@
 package com.titin.firebasecrud
 
-class FirebaseCrudApp {
+import android.app.Application
+import com.google.firebase.database.FirebaseDatabase
+import com.titin.firebasecrud.di.AppContainer
+
+class FirebaseCrudApp : Application() {
+    // Lazy init AppContainer
+    val appContainer by lazy { AppContainer.getInstance(this) }
+
+    override fun onCreate() {
+        super.onCreate()
+        // Habilitar persistencia offline de Firebase
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
+    }
 }
--- app/src/main/java/com/titin/firebasecrud/di/AppContainer.kt
+++ app/src/main/java/com/titin/firebasecrud/di/AppContainer.kt
@@ -1,4 +1,30 @@
 package com.titin.firebasecrud.di
 
-class AppContainer {
+import android.annotation.SuppressLint
+import android.content.Context
+import com.titin.firebasecrud.data.repository.UploadRepository
+import com.titin.firebasecrud.data.source.FirebaseDataSource
+
+class AppContainer(private val context: Context) {
+    // Fuentes de datos
+    val firebaseDataSource = FirebaseDataSource()
+
+    // Repositorios
+    val uploadRepository: UploadRepository by lazy {
+        UploadRepository(firebaseDataSource)
+    }
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        @Volatile
+        private var INSTANCE: AppContainer? = null
+
+        fun getInstance(context: Context): AppContainer {
+            return INSTANCE ?: synchronized(this) {
+                val instance = AppContainer(context.applicationContext)
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
